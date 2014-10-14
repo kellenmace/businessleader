9940f8f8f1d2710d551d44c3c2586a4a9b682f32
Kellen Mace <kellen.mace@gmail.com>
2014-10-14 00:52:17 -0400

    styled archive, 404 and search pages

--- archive.php
+++ archive.php
@@ -9,11 +9,58 @@
 
 get_header(); ?>
 
+<div class='featured-image-banner'>
+	<header class="page-header">
+		<h1 class="page-title">
+			<?php
+				if ( is_category() ) :
+				    printf( __( 'Filed under ', 'bus_leader' ) );
+				    echo '<em>';
+				    single_cat_title();
+				    echo '</em>';
+
+				elseif ( is_tag() ) :
+				    printf( __( 'Tagged as ', 'bus_leader' ) );
+				    echo '<em>';
+				    single_tag_title();
+				    echo '</em> ' . __('tag', 'bus_leader');
+
+				elseif ( is_author() ) :
+					printf( __( 'Author: %s', 'bus_leader' ), '<span class="vcard">' . get_the_author() . '</span>' );
+
+				elseif ( is_day() ) :
+					printf( __( 'Day: %s', 'bus_leader' ), '<span>' . get_the_date() . '</span>' );
+
+				elseif ( is_month() ) :
+					printf( __( 'Month: %s', 'bus_leader' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'bus_leader' ) ) . '</span>' );
+
+				elseif ( is_year() ) :
+					printf( __( 'Year: %s', 'bus_leader' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'bus_leader' ) ) . '</span>' );
+
+				elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+					_e( 'Asides', 'bus_leader' );
+
+				else :
+					_e( 'Archives', 'bus_leader' );
+
+				endif;
+			?>
+		</h1>
+		<?php
+			// Show an optional term description.
+			$term_description = term_description();
+			if ( ! empty( $term_description ) ) :
+				printf( '<div class="taxonomy-description">%s</div>', $term_description );
+			endif;
+		?>
+	</header><!-- .page-header -->
+</div><!-- .featured-image-banner -->
+
 	<?php
 	// TODO: add upload option & integrate customizer
     // if ( has_post_thumbnail( $post->ID ) ) {
 	// $featured_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large-thumb' )[0];
-		$featured_image_url = get_template_directory_uri() . '/images/default-featured-image.jpg';
+	$featured_image_url = get_template_directory_uri() . '/images/default-featured-image.jpg';
 	?>
 	<div class="single-post-featured-image" style="background-image: url('<?php echo $featured_image_url ?>')">
 	</div><!-- .single-post-featured-image -->
@@ -23,50 +70,7 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-						    printf( __( 'Filed under ', 'bus_leader' ) );
-						    echo '<em>';
-						    single_cat_title();
-						    echo '</em> ' . __('category', 'bus_leader');
-
-						elseif ( is_tag() ) :
-						    printf( __( 'Tagged as ', 'bus_leader' ) );
-						    echo '<em>';
-						    single_tag_title();
-						    echo '</em> ' . __('tag', 'bus_leader');
-
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'bus_leader' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'bus_leader' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'bus_leader' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'bus_leader' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'bus_leader' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'bus_leader' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'bus_leader' );
-
-						else :
-							_e( 'Archives', 'bus_leader' );
-
-						endif;
-					?>
-				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .page-header -->
+			
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -76,7 +80,12 @@ get_header(); ?>
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+					if ( 'aside' == get_post_format() ) {
+						get_template_part( 'partials/content', 'aside' );
+					}
+					else {
+						get_template_part( 'content', get_post_format() );
+					}
 				?>
 
 			<?php endwhile; ?>
--- partials/content-none.php
+++ partials/content-none.php
@@ -10,45 +10,47 @@
 
 <section class="<?php if ( is_404() ) { echo 'error-404'; } else { echo 'no-results'; } ?> not-found">
     <div class="index-box">
-	<header class="entry-header">
-		<h1 class="entry-title">
-                    <?php 
-                    if ( is_404() ) { _e( 'Page not available', 'bus_leader' ); }
-                    else if ( is_search() ) { printf( __( 'Nothing found for <em>', 'bus_leader') . get_search_query() . '</em>' ); }
-                    else { _e( 'Nothing Found', 'bus_leader' );}
-                    ?>
-                </h1>
-	</header>
+    	<header class="entry-header">
+    		<h1 class="entry-title">
+                        <?php 
+                        if ( is_404() ) { _e( 'Page not available', 'bus_leader' ); }
+                        else if ( is_search() ) { printf( __( 'Nothing found for <em>', 'bus_leader') . get_search_query() . '</em>' ); }
+                        else { _e( 'Nothing Found', 'bus_leader' );}
+                        ?>
+                    </h1>
+    	</header>
 
-	<div class="entry-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+    	<div class="entry-content">
+    		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'bus_leader' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-                        
-                <?php elseif ( is_404() ) : ?>
-                        
-                        <p><?php _e( 'Sorry — that page can&rsquo;t be found. You can view the most recent articles below or try a search:', 'bus_leader' ); ?></p>
-                        <?php get_search_form(); ?>
-                        
-		<?php elseif ( is_search() ) : ?>
+    			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'bus_leader' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+                            
+                    <?php elseif ( is_404() ) : ?>
+                            
+                            <p><?php _e( 'Sorry — that page can&rsquo;t be found. You can view the most recent articles below or try a search:', 'bus_leader' ); ?></p>
+                            <?php get_search_form(); ?>
+                            
+    		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry — nothing matched your search terms. You can view the most recent articles below or try a search:', 'bus_leader' ); ?></p>
-			<?php get_search_form(); ?>
+    			<p><?php _e( 'Sorry — nothing matched your search terms. You can view the most recent articles below or try a search:', 'bus_leader' ); ?></p>
+    			<?php get_search_form(); ?>
 
-		<?php else : ?>
+    		<?php else : ?>
 
-			<p><?php _e( 'Sorry — it seems we can’t find what you’re looking for. Perhaps searching can help.', 'bus_leader' ); ?></p>
-			<?php get_search_form(); ?>
+    			<p><?php _e( 'Sorry — it seems we can’t find what you’re looking for. Perhaps searching can help.', 'bus_leader' ); ?></p>
+    			<?php get_search_form(); ?>
 
-		<?php endif; ?>
-	</div><!-- .entry-content -->
+    		<?php endif; ?>
+    	</div><!-- .entry-content -->
     </div><!-- .index-box -->
     
     <?php
     if ( is_404() || is_search() ) {
         
         ?>
-    <header class="page-header"><h1 class="page-title">Recent posts:</h1></header>
+    <div class="index-box content-none-page-header">
+        <header class="page-header"><h1 class="page-title">Recent posts:</h1></header>
+    </div><!-- .index-box -->
     <?php
         // Get the 6 latest posts
         $args = array(
@@ -62,8 +64,13 @@
                 while ( $latest_posts_query->have_posts() ) {
 
                     $latest_posts_query->the_post();
-                    // Get the standard index page content
-                    get_template_part( 'content', get_post_format() );
+                    
+                    if ( 'aside' == get_post_format() ) {
+                        get_template_part( 'partials/content', 'aside' );
+                    }
+                    else {
+                        get_template_part( 'content', get_post_format() );
+                    }
 
                 }
         }
