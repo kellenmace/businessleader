eac969565aeb1deac89958eff30fd8f74f4564cd
Kellen Mace <kellen.mace@gmail.com>
2014-10-15 01:15:24 -0400

    removed old js files

--- js/hide-search.js
+++
@@ -1,9 +0,0 @@
-/* 
- * Apply parallax effect to featured image
- */
-jQuery(document).ready(function($){
-    $(window).scroll(function(e){
-        var scrolled = $(window).scrollTop();
-        $('.header-image').css('top', -(scrolled * 0.3) + 'px');
-    });
-});
